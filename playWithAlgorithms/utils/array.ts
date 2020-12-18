ca6e1f05d456c5b74213d19ce035062028735233
ChangerHe <changerheee@gmail.com>
2020-12-18 13:28:35 +0800

    chore: add annotation

--- playWithAlgorithms/utils/array.ts
+++ playWithAlgorithms/utils/array.ts
@@ -7,6 +7,12 @@ export const randomArrayGenerator = (length: number, range: [number, number]) =>
   return [...new Array(length)].map(() => Math.floor(Math.random() * (range[1] - range[0] + 1)) + range[0])
 }
 
+/**
+ * record the function run spend time
+ * @param func function want to record
+ * @param arr test array
+ * @param symbol the only symbol of the function
+ */
 export const timeSpendRecord = (func: (arr: number[]) => number[], arr: number[], symbol: string) => {
   const prevTime = new Date().getTime();
   const newArr = func(arr);
