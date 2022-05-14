7c0365273dcca4cc492c9fc45a91400a74d38fe0
ChangerHe <changerheee@gmail.com>
2022-05-14 20:17:45 +0800

    feat: 206

--- leetcode/206.反转链表.js
+++ leetcode/206.反转链表.js
@@ -16,20 +16,33 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// var reverseList = function (head) {
+//   if (!head) {
+//     return null;
+//   }
+//   let h = {
+//     val: head.val,
+//     next: null,
+//   };
+//   while (head.next) {
+//     h = {
+//       val: head.next.val,
+//       next: h,
+//     };
+//     head = head.next;
+//   }
+//   return h;
+// };
 var reverseList = function (head) {
-  if (!head) {
-    return null;
-  }
-  let h = {
-    val: head.val,
-    next: null,
-  };
-  while (head.next) {
-    h = {
-      val: head.next.val,
-      next: h,
-    };
-    head = head.next;
+  let h = null;
+  let p = head;
+  while (p) {
+    // 官方题解解法: 用一个临时变量来缓存之后的链, 然后将当前链节点插入新链
+    // 然后再迭代之后的链
+    const next = p.next;
+    p.next = h;
+    h = p;
+    p = next;
   }
   return h;
 };
