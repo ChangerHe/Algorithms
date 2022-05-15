e3f4c460c30a05ed179b9ad39086cf9768dadc59
ChangerHe <changerheee@gmail.com>
2022-05-15 21:58:28 +0800

    feat: 239

--- leetcode/239.test.js
+++ leetcode/239.test.js
@@ -5,5 +5,8 @@ describe("", () => {
     expect(maxSlidingWindow([9, 10, 9, -7, -4, -8, 2, -6], 5)).toEqual([
       10, 10, 9, 2,
     ]);
+    expect(maxSlidingWindow([-7, -8, 7, 5, 7, 1, 6, 0], 4)).toEqual([
+      7, 7, 7, 7, 7,
+    ]);
   });
 });
