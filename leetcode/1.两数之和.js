dc8945275c2125c0c3ab2a824b3888580260f0fa
ChangerHe <changerheee@gmail.com>
2023-03-26 09:15:48 +0800

    feat: update 1

--- leetcode/1.两数之和.js
+++ leetcode/1.两数之和.js
@@ -10,17 +10,28 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  for (let index = 0; index < nums.length; index++) {
-    const num = nums[index];
+var twoSum = function (nums, target) {
+  // // 解法1: 使用两次遍历来确定数组的的两个目标下标值
+  // for (let i = 0; i < nums.length; i++) {
+  //   const num1 = nums[i];
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     const num2 = nums[j];
+  //     if (num1 + num2 === target) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+  // 解法2: 利用hashmap的能力, 将一维数组转为map结构后再检索
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
     const restNum = target - num;
-    const restNumPos = nums.indexOf(restNum, index + 1)
-    if (restNumPos > -1) {
-      return [index, restNumPos]
+    if (map.has(restNum)) {
+      return [map.get(restNum), i];
     }
+    map.set(num, i);
   }
 };
 
 module.exports = twoSum;
 // @lc code=end
-
